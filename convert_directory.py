d41190f273232b3f7a8f93f7cac7eda29d8b242e
justineDelo <ju.nyc@hotmail.fr>
2017-09-12 17:24:26 +0200

    maj

--- convert_directory.py
+++ convert_directory.py
@@ -10,7 +10,7 @@ clip_len = 10 		#length of clips for training. Defined in seconds
 block_size = freq / 4 #block sizes used for training - this defines the size of our input state
 max_seq_len = int(round((freq * clip_len) / block_size)) #Used later for zero-padding song sequences
 #Step 1 - convert MP3s to WAVs
-#new_directory = convert_folder_to_wav(input_directory, freq)
+new_directory = convert_folder_to_wav(input_directory, freq)
 #Step 2 - convert WAVs to frequency domain with mean 0 and standard deviation of 1
-new_directory= input_directory+"wave/"
+#new_directory= input_directory+"wave/"
 convert_wav_files_to_nptensor(new_directory, block_size, max_seq_len, output_filename)
