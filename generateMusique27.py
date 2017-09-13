27d430b236133fd1dc9bc5068b2ba6cfb9385eca
justineDelo <ju.nyc@hotmail.fr>
2017-09-13 14:43:31 +0200

    last commit

--- generateMusique27.py
+++ generateMusique27.py
@@ -20,22 +20,21 @@ def retrieveNumbers() :
 def createSounds(path, ideaNb=1) :
     closestsNb=retrieveNumbers()
     compteur=0
+    print (closestsNb[1])
     while len(closestsNb)>0 :
         compteur+=1
         output_filename = path+"sound"+str(compteur)+".wav"
         print(output_filename)
         if ideaNb == 1 :
             if len(closestsNb)>=3 :
-    #            if ideaNb== 3 :
-    #                
-    #                command= "ffmpeg -i concat:"+indice(closestsNb[0])[1]+"|"+indice(closestsNb[1])[1]+"|"+indice(closestsNb[2])[1]+" -acodec copy melange.mp3"
-    #                subprocess.call(command, shell=True)
-    #                generate.main(output_filename,1, )
                 generate.main(output_filename, 3, indice(closestsNb[0])[0], indice(closestsNb[1])[0], indice(closestsNb[2])[0])
                 closestsNb=closestsNb[3:]
             elif len(closestsNb)==2 :
+                print("ici")
                 generate.main(output_filename, 2, indice(closestsNb[0])[0], indice(closestsNb[1])[0])
+                print("done")
                 closestsNb=closestsNb[2:]
+                print("end")
             elif len(closestsNb)==1 :
                 generate.main(output_filename, 1, indice(closestsNb[0])[0])
                 closestsNb=closestsNb[1:]
@@ -48,15 +47,17 @@ def createSounds(path, ideaNb=1) :
 
 
 def indice(couple) :
-    
-    videoNb=couple[0]
-    extractNb=couple[1]
+    print(couple)
+    videoNb=couple[0]+1
+    extractNb=couple[1]+1
     soundName="v"+str(videoNb)+"sound"+str(extractNb)+".wav"
     f=open("filesNames", "rb")
     filesNames=pickle.load(f)
     f.close()
-    directory = filesNames[0]
-    filesNames=filesNames[1:]
+    directory = filesNames[-1]
+    print("dir", directory)
+    filesNames=filesNames[:-1]
+    print(filesNames[0])
     index=filesNames.index(directory+soundName)
     
     return index, directory+soundName
